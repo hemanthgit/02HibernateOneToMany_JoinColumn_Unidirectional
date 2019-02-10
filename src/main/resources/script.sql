20e92ac570e5a4bcab37a16924a4c03853c30846
Hemanth <nagahemanthm@gmail.com>
2019-02-10 14:33:26 +0530

    Update script.sql
    adding post_post_comment table sql for unidirectional one-to-many

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -30,3 +30,26 @@ CREATE SEQUENCE TEST_SEQUENCE
 --------------------------------------------------------
   ALTER TABLE "POST_COMMENT" ADD CONSTRAINT "POST_COMMENT_PK" PRIMARY KEY ("ID");
   ALTER TABLE "POST_COMMENT" MODIFY ("ID" NOT NULL ENABLE);
+                                                                                              
+                                                                                              --------------------------------------------------------
+--  DDL for Table POST_POST_COMMENT
+--------------------------------------------------------
+
+  CREATE TABLE "POST_POST_COMMENT" 
+   (	"POST_ID" NUMBER(20,0), 
+		"COMMENTS_ID" NUMBER(20,0)
+   ) ;
+--------------------------------------------------------
+--  Constraints for Table POST_POST_COMMENT
+--------------------------------------------------------
+
+  ALTER TABLE "POST_POST_COMMENT" MODIFY ("COMMENTS_ID" NOT NULL ENABLE);
+  ALTER TABLE "POST_POST_COMMENT" MODIFY ("POST_ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Ref Constraints for Table POST_POST_COMMENT
+--------------------------------------------------------
+
+  ALTER TABLE "POST_POST_COMMENT" ADD CONSTRAINT "POST_POST_COMMENT_FK1" FOREIGN KEY ("POST_ID")
+	  REFERENCES "POST" ("ID") ENABLE;
+  ALTER TABLE "POST_POST_COMMENT" ADD CONSTRAINT "POST_POST_COMMENT_FK2" FOREIGN KEY ("COMMENTS_ID")
+	  REFERENCES "POST_COMMENT" ("ID") ENABLE;
